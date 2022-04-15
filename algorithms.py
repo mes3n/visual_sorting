282cc6575b099f872e81b79b08684f384f4c366c
mes3n <skidmackan@gmail.com>
2022-04-15 21:34:03 +0200

    added comments

--- algorithms.py
+++ algorithms.py
@@ -7,25 +7,29 @@ def simple_bubble_sort_time(*sortable_list):
     start = time()
 
     sortable_list = list(sortable_list)
+    # when using the threading library passing a list causes it to pass as *args
+    # as such the *args need to be repacked and then turned into a list
 
     while True:
 
+        # find and select a value with a smaller neighbor to the right
         for index, value in enumerate(sortable_list):
             if index + 1 != len(sortable_list):  # end of list
                 if value > sortable_list[index + 1]:  # next number is smaller
                     current_value = value
                     current_index = index
                     break
-            else:
-                return round((time() - start)*1000, 4)
+            else:  # no smaller value can be found
+                return round((time() - start)*1000, 4)  # convert to ms and round to 4 decimals
 
-        while current_index + 1 != len(sortable_list):
+        while current_index + 1 != len(sortable_list):  # not at the end of list
             if current_value > sortable_list[current_index + 1]:
 
                 # remove element and insert it at the position after
                 # also update affected elemets' properties
 
-                current_index += 1
+                current_index += 1  # move value to the right
+                # move the value to the new index
                 sortable_list.remove(current_value)
                 sortable_list.insert(current_index, current_value)
             else:
@@ -36,12 +40,14 @@ def simple_selection_sort_time(*sortable_list):
     start = time()
 
     sortable_list = list(sortable_list)
+    # when using the threading library passing a list causes it to pass as *args
+    # as such the *args need to be repacked and then turned into a list
 
     for i in range(len(sortable_list)):
         # select the smallest value of the segment and find a column with the same value
         # then place it at the front of the searched part of the list
 
-        selected = min([value for value in sortable_list[i:]])
+        selected = min([value for value in sortable_list[i:]])  # select the smallest value
         for value in sortable_list[i:]:
             if value == selected:
                 current_value = value
@@ -59,10 +65,13 @@ def simple_merge_sort_time(*sortable_list):
     start = time()
 
     sortable_list = list(sortable_list)
+    # when using the threading library passing a list causes it to pass as *args
+    # as such the *args need to be repacked and then turned into a list
 
     def merge(first_sublist, second_sublist):
         final  = []
 
+        # take the smallest value of the sublists and append them to the final
         for i in range(len(first_sublist + second_sublist)):
             if not len(first_sublist) and len(second_sublist):
                 val = second_sublist[0]
@@ -86,6 +95,8 @@ def simple_merge_sort_time(*sortable_list):
         return final
 
     def split(split_list):
+
+        # return the lists with elements ordered from smallest -> largest
         if len(split_list) == 1:
             return split_list
 
@@ -96,8 +107,9 @@ def simple_merge_sort_time(*sortable_list):
             else:
                 return split_list
 
+        # find the middle index
         split_index = len(split_list) // 2 + (1 if len(split_list)%2 else 0)
         return merge(split(split_list[:split_index]), split(split_list[split_index:]))
 
-    split(sortable_list)
+    sortable_list = split(sortable_list)  # start recurssion
     return round((time() - start)*1000, 4)
--- main.py
+++ main.py
@@ -5,10 +5,12 @@ import pygame
 from random import randint
 from time import sleep
 
+# run sorting algorithm separatly from main thread
 from multiprocessing.pool import ThreadPool
 pool = ThreadPool(processes=1)
 async_result = pool.apply_async(lambda: 0)
 
+# impor the basic algorithms
 from algorithms import *
 
 pygame.init()
@@ -17,26 +19,27 @@ WIDTH, HEIGHT = 700, 600
 
 ARRAY_SIZE = WIDTH  # must be 0 <= x <= WIDTH
 
-pause = True
-
-function_time = 0
+pause = True  # if visual pauses are enabled
 
 app = pygame.display.set_mode((WIDTH, HEIGHT))  # create window
 pygame.display.set_caption("Sorting")
 font = pygame.font.Font(pygame.font.get_default_font(), 24)
 
+# constant colors
 white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
 
+# values for each column
 class ColumnEntity():
     def __init__(self, value, index):
         self.value = value
         self.index = index
         self.color = black
 
+# class for the comlete list of columns
 class ClassColumns():
     def __init__(self, values):
         self._list = [ColumnEntity(val, i) for i, val in enumerate(values)]  # each passed number becomes a colum
@@ -47,10 +50,9 @@ class ClassColumns():
 
     def bubble_sort(self):  # sort based of bubble sort algorithm
         while True:
-            if check_events() == 'stop': return
+            if check_events() == 'stop': return  # exit if list is reset
 
-            current_entity = None
-
+            # find a colum which is larger than the one next to it
             for index, entity in enumerate(self._list):
                 if index+1 != len(self._list):  # end of list
                     if entity.value > self._list[index+1].value:  # next number is smaller
@@ -59,30 +61,35 @@ class ClassColumns():
                         break
 
             if current_entity:
-                current_entity.color = red
+                current_entity.color = red  # color selected column
 
                 if pause: sleep(0.03)  # visual
 
-                while current_index+1 != len(self._list):
+                # move colum by one step until it is smaller than the column next to it
+                while current_index+1 != len(self._list):  # stay within list
                     if check_events() == 'stop': return
+                    # move colum until it is smaller than the one next to it
                     if current_entity.value > self._list[current_index+1].value:
 
                         # remove element and insert it at the position after
                         # also update affected elemets' properties
 
-                        current_entity.color = red
-                        current_index += 1
+                        # insert colum at selcted spot
+
+                        current_index += 1  # move one step to the right
                         self._list.remove(current_entity)
                         self._list.insert(current_index, current_entity)
 
                         # if pause: sleep(0.01)  # much more illustrative
 
-                    else:
+                    else:  # it is
                         update()
                         break
+
                 current_entity.color = blue
 
             else:
+                # color all finished entities green
                 for entity in self._list:
                     entity.color = green
                     update()
@@ -98,7 +105,8 @@ class ClassColumns():
             # select the smallest value of the segment and find a column with the same value
             # then place it at the front of the searched part of the list
 
-            selected = min([entity.value for entity in self._list[i:]])
+            selected = min([entity.value for entity in self._list[i:]])  # find smallest value
+            # select the entity
             for entity in self._list[i:]:
                 if entity.value == selected:
                     current_entity = entity
@@ -109,6 +117,7 @@ class ClassColumns():
 
             if pause: sleep(0.03)  # can see what is happening
 
+            # move the selected entity to the start of the working list
             if current_entity:
                 self._list.remove(current_entity)
                 self._list.insert(i, current_entity)
@@ -117,7 +126,7 @@ class ClassColumns():
                 current_entity.color = blue
                 update()
 
-        else:
+        else:  # when done color them green
             for entity in self._list:
                 entity.color = green
                 update()
@@ -127,39 +136,40 @@ class ClassColumns():
 
     def merge_sort(self):
         def merge(first_sublist, second_sublist):
-            if check_events() == 'stop': main()
+            if check_events() == 'stop': main()  # exit merge_sort if r is pressed
 
             final  = []
             joined_list = first_sublist + second_sublist
 
+            # color active elements red
             for entity in joined_list:
                 entity.color = red
             update()
 
-            for entity in joined_list:
+            for entity in joined_list:  # select the smallest number from sublist
+                # only one of the sublists holds a value
                 if not len(first_sublist) and len(second_sublist):
-                    val = second_sublist[0]
-                    second_sublist.remove(val)
+                    val = second_sublist[0]  # select the smallest value
+                    second_sublist.remove(val)  # remove from list
                 elif not len(second_sublist) and len(first_sublist):
                     val = first_sublist[0]
                     first_sublist.remove(val)
+                # both sublists hold values
                 else:
-                    if first_sublist[0].value < second_sublist[0].value:
-                        val = first_sublist[0]
+                    if first_sublist[0].value <= second_sublist[0].value:
+                        val = first_sublist[0]  #
                         first_sublist.remove(val)
                     elif second_sublist[0].value < first_sublist[0].value:
                         val = second_sublist[0]
                         second_sublist.remove(val)
-                    elif first_sublist[0].value == second_sublist[0].value:
-                        val = first_sublist[0]
-                        first_sublist.remove(val)
 
-                final.append(val)
+                final.append(val)  # append the smallest value of sublist to final
 
+            # lower and upper indexes for the sublists in correlation to the source list
             lower = min([self._list.index(entity) for entity in joined_list])
             upper = lower + len(final)
 
-            if pause: sleep(len(final)/100)
+            if pause: sleep(len(final)/100)  # sleep more as active lists get larger
 
             self._list[lower:upper] = final
             for entity in final:
@@ -173,9 +183,11 @@ class ClassColumns():
         def split(split_list):
             if check_events() == 'stop': main()
 
+            # simply return list
             if len(split_list) == 1:
                 return split_list
 
+            # return list in order of small -> large
             if len(split_list) == 2:
                 if split_list[0].value > split_list[1].value:
                     return split_list[::-1]
@@ -183,11 +195,13 @@ class ClassColumns():
                 else:
                     return split_list
 
+            # find middle of list then merge the first and second half
             split_index = len(split_list) // 2 + (1 if len(split_list)%2 else 0)
             return merge(split(split_list[:split_index]), split(split_list[split_index:]))
 
-        split(self._list)
+        split(self._list)  # split list in half
 
+        # color final result green
         for entity in self._list:
             entity.color = green
             update()
@@ -199,11 +213,13 @@ class ClassColumns():
 def update():
     app.fill(white)
 
+    # draw each colum
     for index, column in enumerate(columns._list):
         pygame.draw.rect(app, column.color, [blocksizex*index,
                          HEIGHT-blocksizey*column.value, blocksizex, HEIGHT])  # create the values as columns
 
-    if async_result.ready():
+    if async_result.ready():  # see if function is done
+        # draw the return value of the function
         app.blit(font.render(str(async_result.get()) + " ms", True, black), (3, 3))
 
 
@@ -211,8 +227,9 @@ def update():
 
 def check_events(from_main=False):
     global async_result, pause
+
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT:  # closing window
             pygame.quit()
             quit()
         if event.type == pygame.KEYDOWN:
@@ -220,18 +237,21 @@ def check_events(from_main=False):
                 pygame.quit()
                 quit()
             if event.key == pygame.K_SPACE:
-                pause = False if pause else True
+                pause = False if pause else True  # toggle pause
             if event.key == pygame.K_r:
                 columns.__init__([randint(0, 100) for i in range(ARRAY_SIZE)])  # passes random list of integers
-                return 'stop'
+                return 'stop'  # exit merge sort function since its recursive
             if from_main:
                 if event.key == pygame.K_b:
+                    # get time of function by running it in a seperate thread
                     async_result = pool.apply_async(simple_bubble_sort_time, (columns.value_list))  # get time needed
                     columns.bubble_sort()  # sort list visually
                 if event.key == pygame.K_s:
+                    # get time of function by running it in a seperate thread
                     async_result = pool.apply_async(simple_selection_sort_time, (columns.value_list))
                     columns.selection_sort()  # sort list visually
                 if event.key == pygame.K_m:
+                    # get time of function by running it in a seperate thread
                     async_result = pool.apply_async(simple_merge_sort_time, (columns.value_list))
                     columns.merge_sort()  # sort list visually
 
@@ -244,7 +264,7 @@ def main():
 if __name__ == '__main__':
     columns = ClassColumns([randint(0, 100) for i in range(ARRAY_SIZE)])  # create a starting list of random integers
 
-    blocksizex = WIDTH // len(columns._list)
+    blocksizex = WIDTH // len(columns._list)  # one colum is blocksizex wide
     blocksizey = HEIGHT // max(columns.value_list)  # highest value reaches top of window
 
     main()
